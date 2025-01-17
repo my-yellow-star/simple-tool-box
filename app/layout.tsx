93b11a0a7007d38e7a75e83dbf2e15e7b9d1a381
MyYellowStar <c01034784796@gmail.com>
2025-01-17 20:45:19 +0900

    애널리틱스 연동

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,6 +18,8 @@ export const metadata: Metadata = {
   description: "Most Simple & Useful Online Tool Box",
 };
 
+const GA_TRACKING_ID = "G-WC2T7CJHZD";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -24,11 +27,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <meta httpEquiv="Permissions-Policy" content="accelerometer=(self)" />
-      <meta name="google" content="notranslate" />
-      <meta name="robots" content="index, follow" />
-      <meta httpEquiv="Content-Language" content="en" />
-      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <head>
+        <meta httpEquiv="Permissions-Policy" content="accelerometer=(self)" />
+        <meta name="google" content="notranslate" />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen w-screen flex flex-col items-center`}
       >
